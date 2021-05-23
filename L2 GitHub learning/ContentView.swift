a94701c086a70280726ccfec60b630baf2e7d96f
callum day ham <caldayham@gmail.com>
2021-05-22 17:19:56 -0700

    Initial Commit

---
+++ L2 GitHub learning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  L2 GitHub learning
+//
+//  Created by Callum Day Ham on 5/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ L2 GitHub learning/L2_GitHub_learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  L2_GitHub_learningApp.swift
+//  L2 GitHub learning
+//
+//  Created by Callum Day Ham on 5/22/21.
+//
+
+import SwiftUI
+
+@main
+struct L2_GitHub_learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
